331c66d5419c262fb584b9b130c0cc191dee58c9
U <ujjawal.singh@theyarnbazaar.com>
2024-12-20 14:07:31 +0530

    add blog page, navbar, and theme provider; enhance layout and global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import TYBImage from "@/images/tyb.svg";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "The Yarn Bazaar",
+  description: "The Yarn Bazaar is a modern, efficient, and transparently managed marketplace for yarn buyers and sellers to discover real-time yarn prices, information, and market trends.",
+};
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 h-dvh border">
+    <div className="flex flex-col justify-center items-center gap-4 h-full">
       <Image
         src={TYBImage}
         alt="TYB Logo"
---
+++ src/components/nav.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import TYBImage from '@/images/tyb.svg';
+const Navbar: React.FC = () => {
+	return (
+		<nav className="p-4 border hidden md:block">
+			<div className="mx-auto flex justify-between items-center ">
+				<div className="flex items-center space-x-2">
+					<Image
+						src={TYBImage}
+						alt="TYB Logo"
+						width={20}
+						priority
+					/>
+					<div className="text-2xl">The Yarn Bazaar</div>
+				</div>
+				<ul className="flex space-x-4">
+					<li>
+						<Link href="/">Home</Link>
+					</li>
+					<li>
+						<Link href="/blogs">Blogs</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
