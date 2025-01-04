639178b0f88384804d063711652ddb76d7b1e750
Mohd Shamim Khan <shamim.khan@theyarnbazaar.com>
2025-01-04 07:11:43 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: 'build',
-  output: 'export',
+  // distDir: 'build',
+  // output: 'export',
 };
 
 export default nextConfig;
