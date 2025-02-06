b3a7323fcbcd947374cd90e461521b3e0156d5ca
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-06 18:46:54 +0530

    Update footer component layout and add wavy background image

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,10 +4,12 @@ import Image from 'next/image';
 import TYBFullLogo from '@/images/TYBFullLogo.svg';
 import FooterImage from '@/images/FooterImage.png';
 import ArrowImage from '@/images/Arrow.svg';
+import WavyImage from "@/images/about-us/wavesOpacity.svg";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-[#FAE7E2] text-foreground pt-24 flex flex-col relative overflow-hidden mt-8">
+        <footer className="bg-[#FAE7E2] text-foreground pt-44 flex flex-col relative overflow-hidden mt-8">
+            <Image src={WavyImage} alt="Wavy Image" className="opacity w-full absolute top-0 " />
             <div className="flex flex-col gap-8 justify-center items-center">
                 <Image src={TYBFullLogo} alt="company logo" />
                 <nav>
@@ -22,12 +24,7 @@ const Footer: React.FC = () => {
                     </ul>
                 </nav>
                 <Image src={FooterImage} alt="Footer Image" width={1080} />
-                <Image
-                    src={ArrowImage}
-                    alt="mark image"
-                    width={250}
-                    className="absolute top-[430px] -left-10 object-fit"
-                />
+                <Image src={ArrowImage} alt="mark image" width={250} className="absolute top-[430px] -left-10 object-fit" />
             </div>
             <div className='flex border border-[#A3A2A2] justify-between items-center h-14 px-8 mt-[120px]'>
                 <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
@@ -38,12 +35,7 @@ const Footer: React.FC = () => {
                         <li><a href="/terms-and-conditions" className="hover:underline">Terms & Conditions</a></li>
                     </ul>
                 </nav>
-                <Image
-                    src={ArrowImage}
-                    alt="mark image"
-                    width={250}
-                    className="absolute top-5 -right-10 object-fit transform -scale-x-100"
-                />
+                {/* <Image src={ArrowImage} alt="mark image" width={250} className="absolute top-5 -right-10 object-fit transform -scale-x-100" /> */}
             </div>
         </footer>
     );
