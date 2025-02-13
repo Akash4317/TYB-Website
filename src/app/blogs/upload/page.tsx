c53540e46e8e171c1e45624d901d1a7a82ac44a0
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-13 13:05:27 +0530

    Add UploadPage component with metadata for blog uploads

---
+++ src/app/blogs/upload/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+import UploadForm from './form';
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "Upload Blogs - The Yarn Bazaar",
+    description: "Upload blogs to The Yarn Bazaar.",
+  };
+};
+const UploadPage: React.FC = () => {
+
+  return (
+    <UploadForm />
+  );
+};
+
+
+export default UploadPage;
