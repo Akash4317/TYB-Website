72d203de5e5536ae2d32e284ab9a558f3ac33738
@akash.yadav <akash.yadav@theyarnbazaar.com>
2025-02-13 18:00:28 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Hero from "@/components/hero/Hero";
 import Subhero from "@/components/hero/Subhero";
 import Certificate from "@/components/images-scroll/Certificate";
 import Media from "@/components/images-scroll/Media";
-import MobileNav from "@/components/MobileNav";
 import Product from "@/components/product/Product";
 import Progress from "@/components/progress/Progress";
 import Divider from "@/components/ui/divider";
@@ -14,9 +13,6 @@ import Divider from "@/components/ui/divider";
 export default function Home() {
   return (
     <>
-      <div className="block md:hidden mb-[100px]">
-        <MobileNav />
-      </div>
       <Hero />
       <Certificate />
       <Subhero />
