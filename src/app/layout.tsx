54574226e66ac1b1b91b609eece5164cb0065a6c
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-13 15:35:52 +0530

    refactor(layout): remove defaultTheme prop from ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
         <MantineProvider >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
             disableTransitionOnChange
           >
             <Navbar />
