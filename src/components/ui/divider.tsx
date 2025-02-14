a61789ec6fdc2d9d5ae9b9b3c3c0aabfa2b987d4
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-14 23:16:38 +0530

    add className prop to Divider component for customizable styling

--- src/components/ui/divider.tsx
+++ src/components/ui/divider.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import SeperatorImage from '@/images/separator.svg';
+interface DividerProps {
+    className?: string;
+}
 
-const Divider: React.FC = () => {
+const Divider: React.FC<DividerProps> = ({ className }) => {
     return (
-        <div className='flex justify-center items-center gap-4 w-full'>
+        <div className={`flex justify-center items-center gap-4 w-full ${className}`}>
             <Image src={SeperatorImage} alt="Seperator Image" className='w-full'/>
         </div>
     )
