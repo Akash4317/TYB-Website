74609c6cc4c71d8e0ac1afae3c2588184a0409eb
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-17 16:09:42 +0530

    fix: use environment variable for API key in contact form submission

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -29,7 +29,7 @@ const Contact = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'x-api-key': 'bkVcVHsJF36YEQs2o7xOf5Xi4LOY6HX3Axphmrvj'
+          'x-api-key': process.env.LAMBDA_API_KEY || '',
         },
         body: JSON.stringify(formData)
       })
