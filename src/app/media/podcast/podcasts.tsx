4f32d88fe544e1fcbcbb5b3b314fcc267f7a993e
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-17 12:07:08 +0530

    refactor PodcastPage to use custom hook for data fetching and enhance loading/error states

--- src/app/media/podcast/podcasts.tsx
+++ src/app/media/podcast/podcasts.tsx
@@ -1,18 +1,30 @@
+'use client';
+
+import React from 'react';
 import Image from 'next/image';
 
-import { fetchContent } from '@/lambda/db';
-import { contentTypeEnum } from "@/constants/constant";
-import { PodcastInterface } from '@/constants/interface';
-
 import BgLineImage from "@/images/bgLine.svg";
+import { contentTypeEnum } from "@/constants/constant";
+import { useContent } from "@/lib/api";
+import { PodcastInterface } from '@/constants/interface';
+import PodcastDiv from '@/components/media/podcastDiv';
 import YoutubeIcon from '@/icons/youtubeIcon.svg';
 import SpotifyIcon from '@/icons/spoitfyIcon.svg';
-import PodcastDiv from '@/components/media/podcastDiv';
 
+const PodcastPage = () => {
+	const { data, isLoading, isError } = useContent(contentTypeEnum.PODCAST);
 
-const PodcastPage = async () => {
-	const podcasts = await fetchContent(contentTypeEnum.PODCAST);
-
+	if (isLoading) return <div>Loading...</div>;
+	if (isError) return <div>Error loading Blogs</div>;
+	if (!data || data.length === 0) {
+		return (
+			<div className='min-h-[600px] max-w-full flex flex-col justify-center items-center space-y-6 relative w-full bg-[#F8D53C] overflow-hidden'>
+				<Image src={BgLineImage} alt="Background Line" className="absolute -top-10 filter brightness-50 w-full" />
+				<Image src={BgLineImage} alt="Background Line" className="absolute -top-10 filter brightness-50 w-full rotate-2" />
+				<h1 className="font-bold text-4xl">No Blogs available</h1>
+			</div>
+		);
+	}
 	return (
 		<>
 			<div className='min-h-[600px] max-w-full flex flex-col space-y-6 relative w-full bg-[#F8D53C]'>
@@ -22,7 +34,7 @@ const PodcastPage = async () => {
 					<div className='relative overflow-hidden'>
 						<div className="flex justify-evenly flex-wrap gap-8 h-full p-8 z-10">
 							{
-								podcasts && podcasts.map((podcast: PodcastInterface) => (
+								data.map((podcast: PodcastInterface) => (
 									<PodcastDiv key={podcast.id} podcast={podcast} type="secondary" />
 								))
 							}
@@ -33,15 +45,16 @@ const PodcastPage = async () => {
 			<div className='my-8 w-full px-2 md:px-8 flex flex-col gap-2 justify-center'>
 				<a href="https://www.youtube.com/@THEYARNBAZAAR" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline flex items-center gap-2">
 					<Image src={YoutubeIcon} alt="Youtube Icon" className="w-6 h-6 inline-block" />
-					Check all our podcasts on YouTube
+					Check all our podcast in our youtube channel
 				</a>
 				<a href="https://open.spotify.com/show/7nrT5bUoBAtJwuLh9S4F40" target="_blank" rel="noopener noreferrer" className="text-green-600 hover:underline flex items-center gap-2">
 					<Image src={SpotifyIcon} alt="Spotify Icon" className="w-6 h-6 inline-block" />
-					Check all our podcasts on Spotify
+					Check all our podcast in Spotify
 				</a>
 			</div>
+
 		</>
-	);
+	)
 };
 
 export default PodcastPage;
