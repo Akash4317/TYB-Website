55bd0ea0a4518f6a78e9874a56f8bacf4732d1a6
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-17 11:21:47 +0530

    update next.config and package.json; refactor PodcastPage for async data fetching and improved error handling

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
     domains: ['tyb-website-next.s3.ap-south-1.amazonaws.com'],
-  }
+  },
+  distDir: 'build',
 };
 
 export default nextConfig;
