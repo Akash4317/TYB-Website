cb678afe8c46b13d84834e0286e900e0dccd61cd
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-25 18:31:53 +0530

    feat: refactor Scroll component to use structured image data and add link functionality

--- src/components/images-scroll/Scroll.tsx
+++ src/components/images-scroll/Scroll.tsx
@@ -1,33 +1,26 @@
-import Image from 'next/image';
-import React from 'react'
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
+export interface ScrollImagesInterface {
+    src: StaticImageData | string;
+    href: string;
+    alt: string;
+}
 interface ScrollProps {
-    images: string[];
+    data: ScrollImagesInterface[];
 }
 
-const Scroll: React.FC<ScrollProps> = ({ images }) => {
+const Scroll: React.FC<ScrollProps> = ({ data }) => {
     return (
         <div className="pt-12 md:pt-10 box-border pb-8 md:pb-16 md:px-0 px-5">
             <div className="logos relative overflow-hidden flex items-center py-12 px-0 rounded-lg bg-white">
-                <div className="logos-slide flex flex-none whitespace-nowrap animate-scroll">
-                    {images.map((src, idx) => (
+                <div className="logos-slide flex flex-none whitespace-nowrap animate-scroll repeat-infinite">
+                    {data.map((image, idx) => (
                         <div key={idx} className="mx-4">
-                            <Image src={src} alt="brand logo" className="h-[150px] w-auto object-contain" />
-                        </div>
-                    ))}
-                    {images.map((src, idx) => (
-                        <div key={`duplicate-${idx}`} className="mx-4">
-                            <Image src={src} alt="brand logo" className="h-[150px] w-auto object-contain" />
-                        </div>
-                    ))}
-                    {images.map((src, idx) => (
-                        <div key={`duplicate2-${idx}`} className="mx-4">
-                            <Image src={src} alt="brand logo" className="h-[150px] w-auto object-contain" />
-                        </div>
-                    ))}
-                    {images.map((src, idx) => (
-                        <div key={`duplicate3-${idx}`} className="mx-4">
-                            <Image src={src} alt="brand logo" className="h-[150px] w-auto object-contain" />
+                            <Link href={image.href}>
+                                <Image src={image.src} alt={image.alt} className="h-[60px] w-auto object-contain" />
+                            </Link>
                         </div>
                     ))}
                 </div>
