b5bdc2eb8c48587d98278a422af18d3ee126f9af
Ujjawal <Ujjawal.singh@theyarnbazaar.com>
2025-02-25 18:31:49 +0530

    feat: update Media component to use dynamic media coverage data and improve layout

--- src/constants/constant.tsx
+++ src/constants/constant.tsx
@@ -1,4 +1,21 @@
+import { ScrollImagesInterface } from "@/components/images-scroll/Scroll";
 import { LinkInterface } from "./interface";
+import cnbcTv18 from '@/images/media-coverage/cnbc-tv18.png';
+import cnbc from '@/images/media-coverage/cnbc.png';
+import dailyHunt from '@/images/media-coverage/daily-hunt.png';
+import entrepreneurIndia from '@/images/media-coverage/entrepreneur_india.png';
+import hindu from '@/images/media-coverage/hindu.png';
+import iiflSecuritiesLimited from '@/images/media-coverage/iifl_securities_limited.png';
+import inc42 from '@/images/media-coverage/inc_42.png';
+import indianRetailer from '@/images/media-coverage/indian_retailer.png';
+import mint from '@/images/media-coverage/mint.png';
+import moneyControl from '@/images/media-coverage/money-control.png';
+import retailLogoHeader from '@/images/media-coverage/retail-logo-header.png';
+import textileValueChain from '@/images/media-coverage/textile-value-chain.png';
+import theHinduLogo from '@/images/media-coverage/the-hindu-logo.png';
+import yourStory from '@/images/media-coverage/yourstory.png';
+import zeeBusiness from '@/images/media-coverage/zee-business.png';
+
 
 export const links: LinkInterface[] = [
   { url: '/', label: 'Home' },
@@ -54,7 +71,7 @@ export const ProgressCardsInfo = [
     title: "Traceability",
     description:
       "We ensure traceability across our supply chain, enhancing transparency and accountability. By collaborating with partners, we track materials back to their source. This strengthens trust and promotes ethical sourcing.",
-    imageUrl:"https://tyb-website-next.s3.ap-south-1.amazonaws.com/cotton-plant-closeup-field+1.svg",
+    imageUrl: "https://tyb-website-next.s3.ap-south-1.amazonaws.com/cotton-plant-closeup-field+1.svg",
   },
   {
     title: "Sustainability",
@@ -154,17 +171,30 @@ export const sliderCardData = [
 
 ];
 
-export const faqs = [
-  {
-    question: "What is The Yarn Bazaar?",
-    answer:
-      "The Yarn Bazaar is a digital marketplace designed to streamline yarn trading by connecting buyers and sellers with ease, transparency, and efficiency.",
-  },
+export const BuyerFAQ = [
   {
     question: "How can I buy yarn on the platform?",
     answer:
       "Simply browse available yarn listings, compare prices, and place an order directly through our platform. You can also connect with verified sellers for bulk purchases.",
   },
+  {
+    question: "Is The Yarn Bazaar suitable for bulk orders?",
+    answer:
+      "Yes, our platform is ideal for both small and large-scale yarn trading, offering competitive prices and secure transactions for bulk orders.",
+  },
+  {
+    question: "What types of yarn can I trade here?",
+    answer:
+      "You can trade a variety of yarn types, including cotton, polyester, viscose, wool, blended yarns, and more, catering to diverse industry needs.",
+  },
+  {
+    question: "How secure are the transactions on The Yarn Bazaar?",
+    answer:
+      "We ensure secure payments and transactions through verified sellers and buyers, along with trusted payment gateways and escrow services.",
+  },
+]
+
+export const SupplierFAQ = [
   {
     question: "How do I list my yarn for sale?",
     answer:
@@ -186,3 +216,81 @@ export const faqs = [
       "We ensure secure payments and transactions through verified sellers and buyers, along with trusted payment gateways and escrow services.",
   },
 ]
+
+export const mediaCoverageData: ScrollImagesInterface[] = [
+  {
+    src: cnbcTv18,
+    href: "https://www.youtube.com/watch?v=-xgYfAAq49k",
+    alt: "cnbc-logo"
+  },
+  {
+    src: moneyControl,
+    href: "https://www.moneycontrol.com/news/trends/lifestyle/book-excerpt-what-are-shark-tank-india-judges-looking-for-in-a-startup-10298991.html",
+    alt: "money-control"
+  },
+  {
+    src: yourStory,
+    href: "https://yourstory.com/smbstory/b2b-yarn-marketplace-digitising-yarn-textiles-industry",
+    alt: "yourstory"
+  },
+  {
+    src: mint,
+    href: "https://drive.google.com/file/d/1oJ5aZDjvA9YxyWYWc42DGC9WQOWigxjO/view?usp=sharing",
+    alt: "mint"
+  },
+  {
+    src: iiflSecuritiesLimited,
+    href: "https://www.indiainfoline.com/article/general-blog/weekly-musings-big-start-up-updates-for-the-week-to-june--1687235233601_1.html",
+    alt: "economic-retail"
+  },
+  {
+    src: textileValueChain,
+    href: "https://textilevaluechain.in/in-depth-analysis/interviews/start-ups/pratik-the-yarn-bazaar/",
+    alt: "textile-excellence"
+  },
+  {
+    src: dailyHunt,
+    href: "https://m.dailyhunt.in/news/india/english/yourstory-epaper-yourstory/these+businesse[%E2%80%A6]idea+to+succeed+top+smb+stories+of+the+week-newsid-n199746668",
+    alt: "Daily hunt"
+  },
+  {
+    src: cnbc,
+    href: "https://youtu.be/d-Ji7p6yQIk",
+    alt: "cnbc"
+  },
+  {
+    src: hindu,
+    href: "https://www.thehindubusinessline.com/economy/agri-business/online-platform-the-yarn-bazaar-plans-to-take-up-financing-expand-footprint-abroad/article66842574.ece",
+    alt: "money-control"
+  },
+  {
+    src: inc42,
+    href: "https://inc42.com/buzz/the-yarn-bazaar-bags-funding-to-offer-flexible-financing-options-to-sellers/",
+    alt: "Daily hunt"
+  },
+  {
+    src: theHinduLogo,
+    href: "https://www.thehindu.com/business/Industry/yarn-bazaar-raises-funds/article66988897.ece",
+    alt: "money-control"
+  },
+  {
+    src: indianRetailer,
+    href: "https://www.indianretailer.com/news/funding-alert-yarn-bazaar-raises-rs-150-mn-pre-series-round",
+    alt: "cnbc"
+  },
+  {
+    src: entrepreneurIndia,
+    href: "https://www.entrepreneur.com/en-in/news-and-trends/b2b-managed-marketplace-the-yarn-bazaar-secures-inr-15/454347",
+    alt: "entrepreneur"
+  },
+  {
+    src: zeeBusiness,
+    href: "https://www.zeebiz.com/hindi/small-business/the-yarn-bazaar-raises-rs-15-cr-in-pre-series-a-round-who-got-four-shark-deal-in-shark-tank-india-132761",
+    alt: "zee-business"
+  },
+  {
+    src: retailLogoHeader,
+    href: "https://retail.economictimes.indiatimes.com/news/apparel-fashion/apparel/the-yarn-bazaar-raises-rs-150-million-in-pre-series-a-round/101113288",
+    alt: "economic-retail"
+  }
+];
